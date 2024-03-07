d5097267709094495b95c091fe451e31680378fa
HXJ <2532118623@qq.com>
2024-03-07 10:46:22 +0800

    第三次提交

---
+++ azheng-library/admin-library/admin-server/src/main/java/com/xz/library/admin/controller/comm/comm.java
@@ -0,0 +1,13 @@
+package com.xz.library.admin.controller.comm;
+
+/**
+ * @author 黄孝健
+ * @version 1.0
+ * @date 2024/3/7 10:22
+ */
+public class comm {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+        System.out.println("123123132");
+    }
+}
